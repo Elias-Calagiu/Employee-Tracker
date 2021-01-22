f8aca65829c4adc3d050d5c5e518911814af612e
Elias Calagiu <eliasc81@gmail.com>
2021-01-21 21:04:34 -0800

    created employee_tracker_db and tables role and departments

--- schema.sql
+++ schema.sql
@@ -3,3 +3,17 @@ DROP DATABASE IF EXISTS employee_tracker;
 Create DATABASE employee_tracker;
 
 USE DATABASE employee_tracker;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL
+);
+
