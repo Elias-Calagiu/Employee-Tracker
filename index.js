5b48070ced7e9e92d307b44aee5274d8943dc3ca
Elias Calagiu <eliasc81@gmail.com>
2021-01-21 21:59:20 -0800

    add initial inquirer prompt function asking for user input

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const table = require ("console.table")
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employee_tracker_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
+  });
+  
+
+  function init() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+          "View all employees by department",
+          "View all employees by manager",
+          "Add employee",
+          "Remove employee",
+          "Update employee",
+          "Exit"
+        ]
+      })
+      
